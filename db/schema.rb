aeabec406bcbe132959f13e50ec1b69d3e072492
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-03-11 09:50:01 +0300

    add_column state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_10_123456) do
+ActiveRecord::Schema.define(version: 2019_03_11_063702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_03_10_123456) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "state"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
