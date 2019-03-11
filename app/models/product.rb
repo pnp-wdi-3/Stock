eab32967bc2fb55dca09cf411d7fcb47cc5c8e0e
Majeed Almaimoni <MajeedM@Majeeds-MacBook-Pro.local>
2019-03-11 09:59:18 +0300

    did the validation for quantity and price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     belongs_to :user
     validates :name , :brand , :category ,:quantity , :exp_date, :price , presence: true
+    validates :quantity, :price, numericality: {greater_than_or_equal_to: 1 }
 
 end
