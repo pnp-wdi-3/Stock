ed75432ee1ae257757e925912dfc0e4afcb40e6f
Abdulmohsin ALsharhan <mohsin.sharhan@gmail.com>
2019-03-11 11:10:33 +0300

    add triggers for date and tax

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,21 @@ class Product < ApplicationRecord
     belongs_to :user
     validates :name , :brand , :category ,:quantity , :exp_date, :price , presence: true
 
+    before_save :calculate_receipt_tax
+    before_save :define_state
+
+def calculate_receipt_tax
+  tax = self.price * 0.05 + self.price # Whatever you need to do here to calculate
+  self.price = tax
+end
+
+def define_state
+    if self.exp_date < Date.today
+       self.state = "expierd" 
+    else
+        self.state = "new" 
+    end
+  
+  end
+
 end
